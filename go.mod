3806c7852b7b66bc689b5009ef1886f1c16af5d1
DEN SEMENOV <da_semenov@mail.ru>
2022-04-16 16:47:43 +0300

    config

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/da-semenov/gophermart
+
+go 1.17
+
+require (
+	github.com/caarlos0/env/v6 v6.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ internal/app/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"github.com/caarlos0/env/v6"
+	"github.com/spf13/pflag"
+	"os"
+)
+
+type AppConfig struct {
+	ServerAddress        string `env:"RUN_ADDRESS" envDefault:":8080"`
+	DatabaseDSN          string `env:"DATABASE_URI" envDefault:"postgresql://practicum:practicum@127.0.0.1:5432/gophermart"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS" envDefault:"http://localhost:3000"`
+	ReInit               bool   `env:"REINIT" envDefault:"true"`
+}
+
+func (config *AppConfig) Init() error {
+	fmt.Println(os.Args)
+	if err := env.Parse(config); err != nil {
+		fmt.Println("unable to load server settings", err)
+		return err
+	}
+
+	pflag.StringVarP(&config.ServerAddress, "a", "a", config.ServerAddress, "Http-server address")
+	pflag.StringVarP(&config.DatabaseDSN, "d", "d", config.DatabaseDSN, "Database connection string")
+	pflag.StringVarP(&config.AccrualSystemAddress, "r", "r", config.AccrualSystemAddress, "Accrual Service Address")
+	pflag.BoolVarP(&config.ReInit, "c", "c", config.ReInit, "Re-init database")
+	pflag.Parse()
+
+	return nil
+}
+
+func NewConfig() *AppConfig {
+	return &AppConfig{}
+}
