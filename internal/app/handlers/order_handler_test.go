d7e4bf63ddd0c6a887730cd398db5932ab19c311
DEN SEMENOV <da_semenov@mail.ru>
2022-04-23 18:47:38 +0300

    order testing

---
+++ internal/app/handlers/order_handler_test.go
@@ -0,0 +1,210 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"github.com/da-semenov/gophermart/internal/app/domain"
+	"github.com/da-semenov/gophermart/internal/app/handlers/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestOrderHandler_RegisterNewOrder(t *testing.T) {
+	type wants struct {
+		responseCode int
+		contentType  string
+	}
+	type args struct {
+		body      string
+		wantError bool
+		err       error
+	}
+	tests := []struct {
+		name  string
+		wants wants
+		args  args
+	}{
+		{name: "OrderHandler. RegisterNewOrder. Test 1. Positive",
+			wants: wants{
+				responseCode: http.StatusAccepted,
+				contentType:  "application/json",
+			},
+			args: args{
+				body:      "123456",
+				wantError: false,
+				err:       nil,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 2. Empty body",
+			wants: wants{
+				responseCode: http.StatusBadRequest,
+				contentType:  "application/json",
+			},
+			args: args{
+				body:      "",
+				wantError: false,
+				err:       domain.ErrBadParam,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 3. Invalid order num",
+			wants: wants{
+				responseCode: http.StatusUnprocessableEntity,
+				contentType:  "application/json",
+			},
+			args: args{
+				body: "Invalid order num",
+				err:  domain.ErrBadParam,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 4. Order registered early by current user",
+			wants: wants{
+				responseCode: http.StatusOK,
+				contentType:  "application/json",
+			},
+			args: args{
+				body: "123456789",
+				err:  domain.ErrOrderRegistered,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 5. Order registered early by another user",
+			wants: wants{
+				responseCode: http.StatusConflict,
+				contentType:  "application/json",
+			},
+			args: args{
+				body: "123456789",
+				err:  domain.ErrOrderRegisteredByAnotherUser,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 6. Internal error",
+			wants: wants{
+				responseCode: http.StatusInternalServerError,
+				contentType:  "application/json",
+			},
+			args: args{
+				body: "123456789",
+				err:  errors.New("any error"),
+			},
+		},
+	}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	orderService := mocks.NewMockOrderService(mockCtrl)
+	target := NewOrderHandler(orderService, auth, log)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			orderService.EXPECT().Save(ctx, &domain.Order{Num: tt.args.body, UserID: 0}).Return(tt.args.err)
+			body := strings.NewReader(tt.args.body)
+			request := httptest.NewRequest("POST", "/api/user/orders", body)
+			request.Header.Set("Content-Type", "text/plain")
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(target.RegisterNewOrder)
+			h.ServeHTTP(w, request)
+			res := w.Result()
+			contentType := res.Header.Get("Content-type")
+			assert.Equal(t, tt.wants.responseCode, res.StatusCode, "Expected status %d, got %d", tt.wants.responseCode, res.StatusCode)
+			assert.Equal(t, tt.wants.contentType, contentType, "Expected status %d, got %d", tt.wants.contentType, contentType)
+		})
+	}
+}
+
+func generateOrderList(cnt int, userID int) []domain.Order {
+	var res []domain.Order
+	for i := 0; i < cnt; i++ {
+		var obj domain.Order
+		obj.UserID = userID
+		obj.Num = strconv.Itoa(i)
+		obj.Status = "NEW"
+		obj.Accrual = 12
+		obj.UploadAt = time.Now()
+
+		res = append(res, obj)
+	}
+	return res
+}
+
+func TestOrderHandler_GetOrderList(t *testing.T) {
+	type wants struct {
+		responseCode int
+		contentType  string
+	}
+	type args struct {
+		userID    int
+		objCount  int
+		wantError bool
+		err       error
+	}
+	tests := []struct {
+		name  string
+		wants wants
+		args  args
+	}{
+		{name: "OrderHandler. RegisterNewOrder. Test 1. Positive",
+			wants: wants{
+				responseCode: http.StatusOK,
+				contentType:  "application/json",
+			},
+			args: args{
+				wantError: false,
+				objCount:  5,
+				err:       nil,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 2. No data for response",
+			wants: wants{
+				responseCode: http.StatusNoContent,
+				contentType:  "application/json",
+			},
+			args: args{
+				wantError: false,
+				objCount:  0,
+				err:       nil,
+			},
+		},
+		{name: "OrderHandler. RegisterNewOrder. Test 3. Internal error",
+			wants: wants{
+				responseCode: http.StatusInternalServerError,
+				contentType:  "application/json",
+			},
+			args: args{
+				wantError: false,
+				objCount:  0,
+				err:       errors.New("any error"),
+			},
+		},
+	}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	orderService := mocks.NewMockOrderService(mockCtrl)
+
+	target := NewOrderHandler(orderService, auth, log)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			orderService.EXPECT().
+				GetOrderList(ctx, tt.args.userID).
+				Return(generateOrderList(tt.args.objCount, tt.args.userID), tt.args.err)
+
+			request := httptest.NewRequest("GET", "/api/user/orders", nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(target.GetOrderList)
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			contentType := res.Header.Get("Content-type")
+			assert.Equal(t, tt.wants.responseCode, res.StatusCode, "Expected status %d, got %d", tt.wants.responseCode, res.StatusCode)
+			assert.Equal(t, tt.wants.contentType, contentType, "Expected status %d, got %d", tt.wants.contentType, contentType)
+		})
+	}
+}
---
+++ internal/app/service/order_service_test.go
@@ -0,0 +1,187 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/da-semenov/gophermart/internal/app/domain"
+	"github.com/da-semenov/gophermart/internal/app/models"
+	"github.com/da-semenov/gophermart/internal/app/service/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOrderService_Save(t *testing.T) {
+
+	type args struct {
+		order   *domain.Order
+		repoErr error
+	}
+	type wants struct {
+		wantErr bool
+		error   error
+		anyErr  bool
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wants wants
+	}{
+		{
+			name: "OrderService. Save. Test 1. Empty Order",
+			args: args{
+				order: nil,
+			},
+			wants: wants{
+				wantErr: true,
+				error:   domain.ErrBadParam,
+				anyErr:  false,
+			},
+		},
+		{
+			name: "OrderService. Save. Test 2. Empty user",
+			args: args{
+				order: &domain.Order{},
+			},
+			wants: wants{
+				wantErr: true,
+				error:   domain.ErrBadParam,
+				anyErr:  false,
+			},
+		},
+		{
+			name: "OrderService. Save. Test 3. Order registered early",
+			args: args{
+				order: &domain.Order{Num: "Test 3",
+					UserID: 3,
+				},
+			},
+			wants: wants{
+				wantErr: true,
+				error:   domain.ErrOrderRegistered,
+				anyErr:  false,
+			},
+		},
+		{
+			name: "OrderService. Save. Test 4. Order registered by another user",
+			args: args{
+				order: &domain.Order{Num: "Test 4",
+					UserID: 4,
+				},
+			},
+			wants: wants{
+				wantErr: true,
+				error:   domain.ErrOrderRegisteredByAnotherUser,
+				anyErr:  false,
+			},
+		},
+		{
+			name: "OrderService. Save. Test 5. Unexpected error",
+			args: args{
+				order: &domain.Order{Num: "Test 5",
+					UserID: 5,
+				},
+			},
+			wants: wants{
+				wantErr: true,
+				anyErr:  true,
+			},
+		},
+	}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	ctx := context.Background()
+	orderRepository := mocks.NewMockOrderRepository(mockCtrl)
+	target := NewOrderService(orderRepository, log)
+	orderRepository.EXPECT().GetByNum(ctx, "Test 3").Return(&models.Order{Num: "Test 3", UserID: 3}, nil)
+	orderRepository.EXPECT().GetByNum(ctx, "Test 4").Return(&models.Order{Num: "Test 4", UserID: 30}, nil)
+	orderRepository.EXPECT().GetByNum(ctx, "Test 5").Return(nil, errors.New("any error"))
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			err := target.Save(ctx, tt.args.order)
+			if tt.wants.wantErr {
+				if !tt.wants.anyErr {
+					assert.ErrorIs(t, err, tt.wants.error, "Expected error is %v, got %v", tt.wants.error, err)
+				}
+			} else {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wants.wantErr)
+			}
+		})
+	}
+}
+
+func TestOrderService_Save2(t *testing.T) {
+
+	type args struct {
+		order   *domain.Order
+		repoErr error
+	}
+	type wants struct {
+		wantErr bool
+		error   error
+		anyErr  bool
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wants wants
+	}{
+		{
+			name: "OrderService. Save. Test 6. Successful",
+			args: args{
+				order: &domain.Order{Num: "Test 6",
+					UserID: 6,
+				},
+			},
+			wants: wants{
+				wantErr: false,
+				anyErr:  true,
+			},
+		},
+		{
+			name: "OrderService. Save. Test 7. Unexpected error",
+			args: args{
+				order: &domain.Order{Num: "Test 7",
+					UserID: 7,
+				},
+			},
+			wants: wants{
+				wantErr: true,
+				anyErr:  true,
+			},
+		},
+	}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	ctx := context.Background()
+	orderRepository := mocks.NewMockOrderRepository(mockCtrl)
+	target := NewOrderService(orderRepository, log)
+
+	orderRepository.EXPECT().GetByNum(ctx, gomock.Any()).Return(nil, &models.NoRowFound).AnyTimes()
+	orderRepository.EXPECT().Save(ctx, gomock.Any()).DoAndReturn(
+		func(ctx context.Context, arg *models.Order) error {
+			if arg.Num == "Test 6" {
+				return nil
+			}
+			if arg.Num == "Test 7" {
+				return errors.New("any error")
+			}
+			return nil
+		},
+	).AnyTimes()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := target.Save(ctx, tt.args.order)
+			if tt.wants.wantErr {
+				if !tt.wants.anyErr {
+					assert.ErrorIs(t, err, tt.wants.error, "Expected error is %v, got %v", tt.wants.error, err)
+				}
+			} else if err != nil {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wants.wantErr)
+			}
+		})
+	}
+}
